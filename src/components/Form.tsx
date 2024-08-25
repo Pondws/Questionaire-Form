c49d1528fbdb450a95f7ac30dbf56bb1046345da
pond <watcharamai13@gmail.com>
2024-08-25 10:31:29 +0700

    validate_finish_edit_ui

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -18,6 +18,7 @@ import {
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import AddIcon from '@mui/icons-material/Add';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 interface FormDataProp {
   name: string,
@@ -36,6 +37,7 @@ interface QuestionProp {
   choices: ChoicesProp[],
   errors: boolean
 }
+
 
 export default function Form() {
   const [formData, setFormData] = useState<FormDataProp>(
@@ -65,10 +67,6 @@ export default function Form() {
       ]
     }
   )
-
-  // const [nameError, setNameError] = useState(false)
-  // const [titleError, setTitleError] = useState(false)
-  // const [descriptionError, setDescriptionError] = useState(false)
 
   const handleAddQuestion = () => {
     // setFormData((prev) => {
@@ -255,293 +253,299 @@ export default function Form() {
   }
 
   const validateForm = () => {
-    const formIsValid = false;
-    // if (formData.name.trim() === "") {
-    //   setFormData(true)
-    // } else {
-    //   setFormData(false)
-    // }
+    setFormData((prevFormData) => {
+      const nameEmpty = prevFormData.name.trim() === ""
+      const updatedQuestions = prevFormData.questions.map((question) => {
+        const questionTitleEmpty = question.title.trim() === "";
 
-    // question.choices.map((choice, index) => {
-    //   if (choice.description.trim() === "") {
-    //     setFormData(true)
-    //   } else {
-    //     setFormData(false)
-    //   }
-    // })
+        const updatedChoices = question.choices.map((choice) => {
+          const choiceDescriptionEmpty = choice.description.trim() === "";
 
-    setFormData((prevFormData) => ({
-      ...prevFormData,
-      questions: prevFormData.questions.map((question) =>
-        question.title.trim() === ""
-          ? {
-            ...question,
-            choices: question.choices.map((choice) =>
-              choice.description.trim() === ""
-                ? {
-                  ...choice, errors: true,
-                } : { ...choice, errors: false }
-            )
-          } : question
-      )
-    }))
-}
+          return {
+            ...choice,
+            errors: choiceDescriptionEmpty
+          };
+        });
 
-const handleSubmit = (e: any) => {
-  e.preventDefault()
-  validateForm()
-  console.log('formData: ', formData)
-}
+        return {
+          ...question,
+          errors: questionTitleEmpty,
+          choices: updatedChoices
+        };
+      });
 
-return (
-  <Box>
-    <form onSubmit={handleSubmit}>
-      <AppBar position="static"
-        sx={{
-          boxShadow: "none",
-        }}
-      >
+      return {
+        ...prevFormData,
+        errors: nameEmpty,
+        questions: updatedQuestions
+      };
+    });
+  };
 
-        <Toolbar
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    validateForm()
+    console.log('formData: ', formData)
+  }
+
+  return (
+    <Box>
+      <form onSubmit={handleSubmit}>
+        <AppBar position="static"
           sx={{
-            backgroundColor: "#fff",
-            borderBottom: "1px solid #c2c2c2",
-            justifyContent: 'flex-end'
+            boxShadow: "none",
           }}
         >
-          <Button
+
+          <Toolbar
             sx={{
-              borderColor: "#FF5C00",
-              color: "#FF5C00",
-              '&:hover': {
-                borderColor: "#FF5C00",
-              },
+              backgroundColor: "#fff",
+              borderBottom: "1px solid #c2c2c2",
+              justifyContent: 'flex-end'
             }}
-            variant="outlined"
-            onClick={handleResetForm}
           >
-            CANCEL
-          </Button>
-          <Button
-            sx={{
-              backgroundColor: "#FF5C00",
-              color: "#fff",
-              ml: 1,
-              width: 180,
-              border: "none",
-              '&:hover': {
-                borderColor: "#FF5C10",
-                color: "#fff",
-                backgroundColor: "#db4d00",
-              },
-            }}
-            type='submit'
-          >
-            SAVE
-          </Button>
-        </Toolbar>
-      </AppBar>
-
-      <Card sx={{ m: 3 }}>
-        <CardContent>
-          <Box>
-            <Typography component={"h6"} sx={{
-              fontWeight: "400",
-              fontSize: "20px",
-              mb: 2
-            }}>
-              Questionnaire Detail
-            </Typography>
-            <TextField
-              label="Name*"
-              fullWidth
-              InputProps={{ sx: { borderRadius: 2 } }}
-              value={formData.name}
-              onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-              error={formData.errors ? true : false}
-            />
-          </Box>
-          {formData.errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""}
-          {/* {errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""} */}
-        </CardContent>
-
-        {formData.questions.map((question, index) => (
-          <CardContent
-            sx={{
-              borderTop: "1px solid #c2c2c2",
-            }}
-            key={question.questionId}
-          >
-            <Typography component={"h6"} sx={{
-              fontWeight: "400",
-              fontSize: "20px",
-              pb: 2
-            }}>
-              Question {index + 1}
-            </Typography>
-
-            <Box sx={{ mb: 2 }}>
-              <TextField
-                label="Question*"
-                fullWidth
-                name='question'
-                value={question.title}
-                InputProps={{ sx: { borderRadius: 2 } }}
-                onChange={(e) => handleQuestionChange(question.questionId, e.target.value)}
-                error={question.errors ? true : false}
-              />
-              {question.errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""}
-              {/* {question.title === "" ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""} */}
-              {/* {errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""} */}
-            </Box>
-
-            <RadioGroup>
-              {question.choices.map((choice) => (
-                <Box
-                  key={choice.choiceId}
-                  sx={{
-                    display: "flex",
-                  }}
-                >
-
-                  <Radio
-                    sx={{
-                      color: "#00040C"
-                    }}
-                    name={question.title}
-                    checked={choice.checked}
-                    value={choice.checked}
-                    onChange={() => handleCheckedChange(question.questionId, choice.choiceId)}
-                  />
-
-                  <Box
-                    sx={{
-                      width: "100%",
-                      my: 1
-                    }}
-                  >
-                    <TextField
-                      label="Description*"
-                      fullWidth
-                      name='description'
-                      value={choice.description}
-                      InputProps={{ sx: { borderRadius: 2 } }}
-                      onChange={(e) => handleDescriptionChange(question.questionId, choice.choiceId, e.target.value)}
-                      error={choice.errors ? true : false}
-                    />
-                    {choice.checked
-                      ? <Typography sx={{
-                        fontSize: "12px"
-                      }}>
-                        This answer is correct
-                      </Typography>
-                      : ""
-                    }
-                    {choice.errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""}
-                    {/* {errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""} */}
-                  </Box>
-
-                  <Button
-                    sx={{
-                      color: "#00040C",
-                      p: 0
-                    }}
-                    onClick={() => handleDeleteChoice(question.questionId, choice.choiceId)}
-                  >
-                    <DeleteOutlineIcon />
-                  </Button>
-
-                </Box>
-              ))}
-            </RadioGroup>
             <Button
               sx={{
-                p: 0,
-                fontSize: 14,
+                borderColor: "#FF5C00",
                 color: "#FF5C00",
-                my: 2
+                '&:hover': {
+                  borderColor: "#FF5C00",
+                },
               }}
-              onClick={() => handleAddChoice(question.questionId)}
+              variant="outlined"
+              onClick={handleResetForm}
+            >
+              CANCEL
+            </Button>
+            <Button
+              sx={{
+                backgroundColor: "#FF5C00",
+                color: "#fff",
+                ml: 1,
+                width: 180,
+                border: "none",
+                '&:hover': {
+                  borderColor: "#FF5C10",
+                  color: "#fff",
+                  backgroundColor: "#db4d00",
+                },
+              }}
+              type='submit'
+            >
+              SAVE
+            </Button>
+          </Toolbar>
+        </AppBar>
+
+        <Card sx={{ m: 3 }}>
+          <CardContent>
+            <Box>
+              <Typography component={"h6"} sx={{
+                fontWeight: "400",
+                fontSize: "20px",
+                mb: 2
+              }}>
+                Questionnaire Detail
+              </Typography>
+              <TextField
+                label="Name*"
+                fullWidth
+                InputProps={{ sx: { borderRadius: 2 } }}
+                value={formData.name}
+                onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                error={formData.errors ? true : false}
+                helperText={formData.errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""}
+              />
+            </Box>
+          </CardContent>
+
+          {formData.questions.map((question, index) => (
+            <CardContent
+              sx={{
+                borderTop: "1px solid #c2c2c2",
+              }}
+              key={question.questionId}
+            >
+              <Typography component={"h6"} sx={{
+                fontWeight: "400",
+                fontSize: "20px",
+                pb: 2
+              }}>
+                Question {index + 1}
+              </Typography>
+
+              <Box sx={{ mb: 2 }}>
+                <TextField
+                  label="Question*"
+                  fullWidth
+                  name='question'
+                  value={question.title}
+                  InputProps={{ sx: { borderRadius: 2 } }}
+                  onChange={(e) => handleQuestionChange(question.questionId, e.target.value)}
+                  error={question.errors ? true : false}
+                  helperText={question.errors ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography> : ""}
+                />
+              </Box>
+
+              <RadioGroup>
+                {question.choices.map((choice) => (
+                  <Box
+                    key={choice.choiceId}
+                    sx={{
+                      display: "flex",
+                    }}
+                  >
+
+                    <Radio
+                      sx={{
+                        color: "#00040C",
+                        "&:hover": {
+                          backgroundColor: 'transparent'
+                        },
+                        pl: 0,
+                        width: 50,
+                      }}
+                      name={question.title}
+                      checked={choice.checked}
+                      value={choice.checked}
+                      onChange={() => handleCheckedChange(question.questionId, choice.choiceId)}
+                      checkedIcon={<CheckCircleIcon style={{ fontSize: 24, color: '#00C62B' }} />}
+                    />
+
+                    <Box
+                      sx={{
+                        width: "100%",
+                        my: 1
+                      }}
+                    >
+                      <TextField
+                        label="Description*"
+                        fullWidth
+                        name='description'
+                        value={choice.description}
+                        InputProps={{ sx: { borderRadius: 2 } }}
+                        onChange={(e) => handleDescriptionChange(question.questionId, choice.choiceId, e.target.value)}
+                        error={choice.errors ? true : false}
+                        helperText={choice.errors
+                          ? <Typography sx={{ color: 'error.main', fontSize: 12 }}>Please fill in this option</Typography>
+                          : ""
+                        }
+                      />
+                      {choice.checked
+                        ? <Typography sx={{
+                          fontSize: "12px"
+                        }}>
+                          This answer is correct
+                        </Typography>
+                        : ""
+                      }
+                    </Box>
+
+                    <Box
+                      sx={{
+                        color: "#00040C",
+                        pr: 0,
+                        display: "flex",
+                        justifyContent: "flex-end",
+                        alignItems: "center",
+                        width: 50,
+                      }}
+                      onClick={() => handleDeleteChoice(question.questionId, choice.choiceId)}
+                    >
+                      <DeleteOutlineIcon />
+                    </Box>
+
+                  </Box>
+                ))}
+              </RadioGroup>
+              <Button
+                sx={{
+                  p: 0,
+                  fontSize: 14,
+                  color: "#FF5C00",
+                  my: 2
+                }}
+                onClick={() => handleAddChoice(question.questionId)}
+              >
+                <AddIcon />
+                <Typography sx={{
+                  ml: 1
+                }}>
+                  ADD CHOICE
+                </Typography>
+
+              </Button>
+
+              <Divider sx={{ backgroundColor: "#C2C9D1" }} />
+
+              <Box
+                sx={{
+                  mt: 2
+                }}
+              >
+                <Button sx={{
+                  color: "#00040C",
+                  p: 0
+                }}
+                  onClick={() => handleCopyQuestion(question.questionId)}
+                >
+                  <ContentCopyIcon />
+                  <Typography sx={{
+                    ml: 1
+                  }}>
+                    DUPLICATE
+                  </Typography>
+
+                </Button>
+                <Button sx={{
+                  color: "#00040C",
+                  p: 0,
+                  ml: 2
+                }}
+                  onClick={() => handleDeleteQuestion(question.questionId)}
+                >
+                  <DeleteOutlineIcon />
+                  <Typography sx={{
+                    ml: 1
+                  }}>
+                    DELETE
+                  </Typography>
+
+                </Button>
+              </Box>
+            </CardContent>
+          ))}
+
+          <CardActions
+            sx={{
+              p: 3,
+              borderTop: "1px solid #c2c2c2"
+            }}
+          >
+            <Button
+              variant="outlined"
+              fullWidth
+              sx={{
+                height: 48,
+                borderRadius: 2,
+                borderColor: "#FF5C00",
+                color: "#FF5C00",
+                '&:hover': {
+                  borderColor: "#FF5C00",
+                },
+              }}
+              onClick={handleAddQuestion}
             >
               <AddIcon />
               <Typography sx={{
-                ml: 1
+                pr: 2
               }}>
-                ADD CHOICE
+                ADD QUESTION
               </Typography>
-
             </Button>
-
-            <Divider sx={{ backgroundColor: "#C2C9D1" }} />
-
-            <Box
-              sx={{
-                mt: 2
-              }}
-            >
-              <Button sx={{
-                color: "#00040C",
-                p: 0
-              }}
-                onClick={() => handleCopyQuestion(question.questionId)}
-              >
-                <ContentCopyIcon />
-                <Typography sx={{
-                  ml: 1
-                }}>
-                  DUPLICATE
-                </Typography>
-
-              </Button>
-              <Button sx={{
-                color: "#00040C",
-                p: 0,
-                ml: 2
-              }}
-                onClick={() => handleDeleteQuestion(question.questionId)}
-              >
-                <DeleteOutlineIcon />
-                <Typography sx={{
-                  ml: 1
-                }}>
-                  DELETE
-                </Typography>
-
-              </Button>
-            </Box>
-          </CardContent>
-        ))}
-
-        <CardActions
-          sx={{
-            p: 3,
-            borderTop: "1px solid #c2c2c2"
-          }}
-        >
-          <Button
-            variant="outlined"
-            fullWidth
-            sx={{
-              height: 48,
-              borderRadius: 2,
-              borderColor: "#FF5C00",
-              color: "#FF5C00",
-              '&:hover': {
-                borderColor: "#FF5C00",
-              },
-            }}
-            onClick={handleAddQuestion}
-          >
-            <AddIcon />
-            <Typography sx={{
-              pr: 2
-            }}>
-              ADD QUESTION
-            </Typography>
-          </Button>
-        </CardActions>
-      </Card>
-    </form>
-  </Box >
-)
+          </CardActions>
+        </Card>
+      </form>
+    </Box >
+  )
 }
